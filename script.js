062099fc26df5bb2248d5e7827b45494087dbac1
wanjikuciku <lorna.wanjiku56@gmail.com>
2018-12-01 17:16:11 +0300

    change indentation and spacing

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
   function getTriangle(){
-  var length = (prompt ("Enter the length"));
-  var width = (prompt (" Enter the width"));
-  var height = (prompt ("Enter the height"));
+  var length = (prompt ("Enter the length");
+  var width = (prompt ("Enter the width");
+  var height = (prompt ("Enter the height");
 
-  if (length === width && length === height && (width === height) {
-  alert ("Equilateral: All sides are equal!")
+  if(length===width && length===height && (width===height) {
+  alert("Equilateral: All sides are equal")
   }
-  else if (length === width || length === height || width === height) {
-  alert ("Isosceles: Exactly 2 sides are equal.")
+  else if(length===width || length===height || width===height) {
+  alert("Isosceles: Exactly 2 sides are equal")
   }
-  else if((length + width)<=height||(length + height)<=width ||(width + height)<=length) {
-  alert ("Scalene: No sides are equal.")
+  else if((length+width)<=height || (length+height)<=width || (width+height)<=length) {
+  alert("Scalene: No sides are equal")
   }
-  else {
+  else{
   alert("NOT a triangle.")
   }
   }
