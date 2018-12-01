b2bb2a09bf2a4c02de74cccb52d32360bbe717a0
wanjikuciku <lorna.wanjiku56@gmail.com>
2018-12-01 18:09:55 +0300

    add a readme.md to the project

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-  function getTriangle(){
-  var length = (prompt ("Enter the length");
-  var width = (prompt ("Enter the width");
-  var height = (prompt ("Enter the height");
+var triangleTracker = function(){
+  var length=document.prompt("enter the length")
+  var width=document.prompt("enter the width")
+  var height=document.prompt("enter the height")
 
   if(length===width && length===height && (width===height) {
-  alert("Equilateral: All sides are equal")
+  alert("Equilateral:All sides are equal")
   }
   else if(length===width || length===height || width===height) {
-  alert("Isosceles: Exactly 2 sides are equal")
+  alert("Isosceles:Exactly 2 sides are equal")
   }
   else if((length+width)<=height || (length+height)<=width || (width+height)<=length) {
-  alert("Scalene: No sides are equal")
+  alert("Scalene:No sides are equal")
   }
   else{
-  alert("NOT a triangle.")
-  }
+  alert("NOT a triangle")
   }
