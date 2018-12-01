a6e182bbedea9f5eab6e986b353315748b209c9d
wanjikuciku <lorna.wanjiku56@gmail.com>
2018-12-01 14:49:34 +0300

    add function to script.js

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-$(document).ready(function() {
-  $("form#triangle").submit(function() {
-    event.preventDefault();
-     var side1 = parseInt($("#side1").val());
-    var side2 = parseInt($("#side1").val());
-    var side3 = parseInt($("#side1").val());
-    var result;
-    // if (side1 === side2 === side3) {
-    //   result = "This is an equilateral triangle";
-    // if (side1 === side2) {
-    //   if (side2 === side3) {
-    //     if (side3 === side1) {
-    //       result = "This is an equilateral triangle";
-    //     }
-    //     }
-    //     }
-    // } else if (side1 === side2 ! side3) {
-    //   result = "This is an isosceles triangle";
-    // } else if (side1 ! side2 === side3) {
-    //   result = "This is an isosceles triangle";
-    // } else if (side1 === side3 ! side2) {
-    //   result = "This is an isosceles triangle";
-    }
-    console.log("result " + result);
-    console.log("side1 " + side1);
-    console.log("side2 " + side2);
-    console.log("side3 " + side3);
-     $("#output").text(result);
-  });
-  });
+  function knowtriangle(){
+  var length = prompt ("side length");
+  var width = prompt (" side width");
+  var height = prompt ("side height");
+
+  if (length === width && length === height && (width === height) {
+  alert ("Equilateral: All sides are equal!")
+  }
+  else if (length === width || length === height || width === height) {
+  alert ("Isosceles: Exactly 2 sides are equal.")
+  }
+  else {
+  alert ("Scalene: No sides are equal.")
+  }
+  else {
+  alert("NOT a triangle.")
+  }
+  }
