b2254627402ae41268a3da3633e58b676bea2f38
wanjikuciku <lorna.wanjiku56@gmail.com>
2018-12-03 10:39:07 +0300

    add a function on NaN in the js file

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
   function triangles (){
+
   var length=parseFloat(prompt("enter the length"));
   var width=parseFloat(prompt("enter the width"));
   var height=parseFloat(prompt("enter the height"));
 
-  if(length===width && length===height && width===height){
+  if (isNaN(length) && isNaN(width) && isNaN(height)) {
+  alert("Please input a Number.");
+  }
+  else if(length===width && length===height && width===height){
   alert("equilateral:All sides are equal");
   }
   else if(length===width || length===height || width===height) {
